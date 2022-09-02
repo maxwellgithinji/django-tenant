48c9b9dc0a737bb32fac68d5c4c5633bcb229fae
maxwellgithinji <maxwellgithinji@gmail.com>
2022-09-02 15:49:59 +0300

    feat: setup multitenancy
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

---
+++ app/app/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-(627il5kks*%f#q#zi53@bl&@!+0_v8vl7mu_!@gbd@i_!vhe%"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
